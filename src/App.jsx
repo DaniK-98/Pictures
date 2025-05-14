fd0f9bb5781054f0bca6a2c9931fd417378a78af
DaniK-98 <genesisdanielamartinezcardenas@gmail.com>
2025-05-14 13:30:39 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import searchImages from './api'
 
 function App() {
   //SearchImage('cars')
-  const [Images, setImages] = useState ([])
+  const [images, setImages] = useState ([])
 
   const handleSubmit = async(term) => {
     console.log('Usted esta buscando con' , term)
